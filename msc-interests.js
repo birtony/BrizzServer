1b042f8b6f687443db8340540d3bb460c616df55
Anton <birtony99@gmail.com>
2019-09-22 23:20:52 -0400

    Updated schemas

---
+++ msc-interests.js
@@ -0,0 +1,40 @@
+// Setup
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// User Schema
+module.exports = new Schema({
+  accounting: Boolean,
+  administration: Boolean,
+  acting: Boolean,
+  arts: Boolean,
+  aviation: Boolean,
+  broadcasting: Boolean,
+  business: Boolean,
+  chemistry: Boolean,
+  civilEngineering: Boolean,
+  computerNetworking: Boolean,
+  cosmetics: Boolean,
+  legal: Boolean,
+  programming: Boolean,
+  childhoodEducation: Boolean,
+  electronics: Boolean,
+  fashion: Boolean,
+  fireProtection: Boolean,
+  fitness: Boolean,
+  flightServices: Boolean,
+  hospitality: Boolean,
+  mediaDesign: Boolean,
+  journalism: Boolean,
+  justice: Boolean,
+  law: Boolean,
+  mechanicalEngineering: Boolean,
+  opticianry: Boolean,
+  paralegal: Boolean,
+  photography: Boolean,
+  police: Boolean,
+  nursing: Boolean,
+  socialService: Boolean,
+  tourism: Boolean,
+  veterinary: Boolean
+});
